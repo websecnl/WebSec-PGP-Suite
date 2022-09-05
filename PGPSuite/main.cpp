e32678abf3818c53e74ff064d6550762549c8833
Lewi <privkoen00@gmail.com>
2022-09-05 12:08:02 +0200

    setup basic projectfiles and main

--- PGPSuite/main.cpp
+++ PGPSuite/main.cpp
@@ -1,21 +1,8 @@
 #include <iostream>
-#include <raylib.h>
 
 int main()
 {
-    constexpr int screenWidth = 800;
-    constexpr int screenHeight = 450;
-
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-
-    SetTargetFPS(144);
-
-    while (!WindowShouldClose())
-    {
-
-    }
-
-    CloseWindow();
+    std::cout << "Hello world\n";
 
     return 0;
 }
