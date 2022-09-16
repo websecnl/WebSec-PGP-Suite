3e31f795794576db473353f80d7df9c6cbdc3955
Lewi <privkoen00@gmail.com>
2022-09-16 11:37:05 +0200

    moved decrypt for final project into own files

---
+++ PGPSuite/PGPDecrypt.cpp
@@ -0,0 +1,56 @@
+#include "PGPDecrypt.h"
+
+bool pgp::cin_pass_provider(rnp_ffi_t ffi, void* app_ctx, rnp_key_handle_t key, const char* pgp_context, char buf[], size_t buf_len)
+{
+    std::string input{};
+    if (pgp_context == std::string("decrypt (symmetric)") ||
+        pgp_context == std::string("decrypt"))
+        input = io::prompt_input(pgp_context, ": ");
+
+    if (input.size() > buf_len) return false;
+
+    std::copy(input.begin(), input.end(), buf);
+
+    return input.size() > 0;
+}
+
+bool pgp::decrypt_text(std::string secring_file, std::string encrypted_file, std::string output_fname)
+{
+    rnp::FFI ffi("GPG", "GPG");
+    rnp::Input keyfile;
+    rnp::Input input;
+    rnp::Output output;
+    std::vector<uint8_t> buffer;
+
+    if (output_fname.size() == 0)
+        output_fname = utils::remove_extension(encrypted_file);
+
+    /* load secret keyring, as it is required for public-key decryption. However, you may
+        * need to load public keyring as well to validate key's signatures. */
+    if (keyfile.set_input_from_path(std::forward<std::string>(secring_file))) return false;
+
+    /* we may use RNP_LOAD_SAVE_SECRET_KEYS | RNP_LOAD_SAVE_PUBLIC_KEYS as well*/
+    if (rnp_load_keys(ffi, "GPG", keyfile, RNP_LOAD_SAVE_SECRET_KEYS) != RNP_SUCCESS)
+    {
+        std::cout << "failed to read secring.pgp\n";
+        return false;
+    }
+    keyfile.destroy();
+
+    rnp_ffi_set_pass_provider(ffi, cin_pass_provider, NULL);
+
+    /* create file input and memory output objects for the encrypted message and decrypted
+     * message */
+    if (input.set_input_from_path(std::forward<std::string>(encrypted_file)) != RNP_SUCCESS) return false;
+
+    if (output.set_output_to_path(std::forward<std::string>(output_fname)) != RNP_SUCCESS) return false;
+
+    /* input: where is the encrypted data
+       output: where to save the decrypted data */
+    if (rnp_decrypt(ffi, input, output) != RNP_SUCCESS) {
+        std::cerr << "public-key decryption failed\n";
+        return false;
+    }
+
+    return true;
+}
---
+++ PGPSuite/PGPDecrypt.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "pgpsuite_common.h"
+#include "rnp_wrappers.h"
+#include "IOTools.h"
+#include "Utils.h"
+
+namespace pgp
+{
+    /* should go somewhere else, but alas, he lives here now */
+    bool cin_pass_provider(rnp_ffi_t           ffi,
+        void* app_ctx,
+        rnp_key_handle_t    key,
+        const char* pgp_context,
+        char                buf[],
+        size_t              buf_len);
+
+
+    /* @brief Decrypt files using secret key 
+    @param secring_file: Filename of secret keyring
+    @param encrypted_file: Filename with encrypted file
+    @param output_fname: Filename of the decrypted data,
+    if empty, name will be same as encrypted file minus .asc */
+    bool decrypt_text(std::string secring_file = "secring.pgp", std::string encrypted_file = "message.asc", std::string output_fname = "");
+}
