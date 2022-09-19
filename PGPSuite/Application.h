086944e5aa1cf2f1facdc29eb8aacf6ca9c52243
Lewi <privkoen00@gmail.com>
2022-09-19 15:07:40 +0200

    start on main application

---
+++ PGPSuite/Application.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <raylib.h>
+
+namespace suite
+{
+	class Application
+	{
+	public:
+		Application() = default;
+		
+		/* handling user input */
+		void input()
+		{
+
+		}
+
+		/* updating UI elements */
+		void update()
+		{
+
+		}
+
+		void draw() const
+		{
+
+		}
+	};
+}
