218763c5c777deb11bed7a7a125394334bc99ad4
Lewi <privkoen00@gmail.com>
2022-09-19 12:31:00 +0200

    header file for pgpgeneratekeys.cpp

---
+++ PGPSuite/PGPGenerateKeys.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "pgpsuite_common.h"
+#include "IOTools.h"
+#include <rnp\rnp.h>
+#include "rnp_wrappers.h"
+
+namespace pgp
+{
+	/* should really go somewhere else but, i cba */
+	bool generic_cin_pass_provider(rnp_ffi_t           ffi,
+		void* app_ctx,
+		rnp_key_handle_t    key,
+		const char* pgp_context,
+		char                buf[],
+		size_t              buf_len);
+
+	/* @brief Generate a keyring with settings from a json file
+	@param pubkey_file: filename for public keyring
+	@param secret_file: filename for secret keyring
+	@param key_data: filename for json file holding keydata */
+	bool generate_keys(std::string pubkey_file = "pubring.pgp", std::string secret_file = "secring.pgp", std::string key_data = "keygen.json");
+}
+
