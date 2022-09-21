7e3d465bdaa4d533f6f9c12d029499613fed4009
Lewi <privkoen00@gmail.com>
2022-09-21 11:08:30 +0200

    split internal virtual functions from exposed functions

--- PGPSuite/Application.h
+++ PGPSuite/Application.h
@@ -2,28 +2,39 @@
 
 #include <raylib.h>
 
+#include "UIElements.h"
+
 namespace suite
 {
 	class Application
 	{
+	protected:
+		ui::UIContainer _ui;
+
+		virtual void on_input() {}
+		virtual void on_update() {}
+		virtual void on_draw() const {}
 	public:
 		Application() = default;
 		
 		/* handling user input */
-		void input()
+		virtual void input()
 		{
-
+			const auto m = GetMousePosition();
+			_ui.update(m);
+			
+			on_input();
 		}
 
 		/* updating UI elements */
 		void update()
 		{
-
+			on_update();
 		}
 
-		void draw() const
+		virtual void draw() const
 		{
-
+			on_draw();
 		}
 	};
 }
