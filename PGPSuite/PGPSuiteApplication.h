d44af28fa84523385488aa709ef050930bf1ed78
Lewi <privkoen00@gmail.com>
2022-09-22 11:49:31 +0200

    small example of the textbox added

--- PGPSuite/PGPSuiteApplication.h
+++ PGPSuite/PGPSuiteApplication.h
@@ -21,7 +21,11 @@ namespace suite
         {
             auto button = _ui.add_element<ui::Button>(Rectangle{ 20.f,20.f,50.f,20.f });
 
-            button->bind(ui::ButtonState::LeftClicked, []() { std::cout << "hello world!\n"; });
+            auto textbox = _ui.add_element<ui::StaticInputBox>(Rectangle{ 20.f,50.f,500.f,40.f }, 10.f);
+
+            auto text = _ui.add_element<ui::DynamicTextBox>(Rectangle{ 20.f,100.f,550.f,40.f }, 30.f, std::string("Hello world! Textbox here!"));
+
+            button->bind(ui::ButtonState::LeftClicked, [textbox, text]() { text->text(textbox->copy_buffer()); });
         }
     public:
         /* @param width: Window width
@@ -29,6 +33,11 @@ namespace suite
         PGPSuiteApplication(int width, int height) 
             : _height(height)
             , _width(width) 
-        { init(); }
+        {}
+
+        void start() override
+        {
+            init();
+        }
     };
 }
