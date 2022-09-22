d2c3fb08e5b1b6bcc583f64a24801efd444c8765
Lewi <privkoen00@gmail.com>
2022-09-22 17:12:44 +0200

    added generate key application and statemachine to access it

--- PGPSuite/PGPSuiteApplication.h
+++ PGPSuite/PGPSuiteApplication.h
@@ -1,38 +1,103 @@
 #pragma once
 
-#include "Application.h"
 #include <iostream>
+#include <memory>
+#include <unordered_map>
+
+#include "Application.h"
 #include "PGPGenerateKeys.h"
 #include "IOTools.h"
 #include "WindowsDialogues.h"
 
 namespace suite
 {
+    enum class SuiteStates { Main, GenerateKey };
+
+    /* GUI to handle key generation */
+    class PGPSuiteKeyGeneration
+        : public Application
+    {
+    protected:
+        int _width{}, _height{};
+    public:
+        PGPSuiteKeyGeneration(int width, int height)
+            : _width(width)
+            , _height(height)
+        {}
+
+        void start() override
+        {
+            _ui.add_element<ui::DynamicTextBox>(Rectangle{ 0.f, 100.f, 200.f,200.f }, 20.f, "Welcome to the key generation window! heere we generate keys");
+        }
+    };
+
     /* @brief Main GUI for simple and quick OpenPGP encryption */
     class PGPSuiteApplication :
         public Application
     {
     protected:
+        using AppPtr = std::shared_ptr<Application>;
+        using StateMachine = std::unordered_map<SuiteStates, AppPtr>;
+
         int _width{}, _height{};
         io::WindowsDialogues _win_diags;
+        SuiteStates _state = SuiteStates::Main;
+        StateMachine _statemachine;
+        ui::Button _back_button;
 
+        bool no_state_active() const { return _state == SuiteStates::Main; }
+        AppPtr active_state() { return _statemachine[_state]; }
+        const AppPtr active_state() const { return _statemachine.at(_state); }
+
+        void on_input() override
+        {
+            if (no_state_active()) return;
+            active_state()->input();
+            _back_button.update(GetMousePosition());
+        }
+        
         void on_update() override
         {
+            if (no_state_active()) return;
+            active_state()->update();
+        }
 
+        void on_draw() const override
+        {
+            if (no_state_active()) return;
+            active_state()->draw();
+            _back_button.draw();
         }
 
         void init()
         {
-            float button_width = 100.f, button_height = 40.f;
-            
-            auto button = _ui.add_element<ui::Button>(Rectangle{ 50.f,50.f, button_width, button_height });
+            auto button = _ui.add_element<ui::Button>(Rectangle{ 50.f,50.f,200.f,100.f });
+            button->bind(ui::ButtonState::LeftClicked, [this]() { switch_state(SuiteStates::GenerateKey); });
+        }
 
-            button->bind(ui::ButtonState::LeftClicked, [this]()
-                {
-                    bool result = pgp::generate_keys();
-                    static std::string success = "Success!", failure = "Failed!", desc = "Saved keys to secring.pgp and keyring.pgp";
-                    _win_diags.simple_pop_up(result ? success : failure, result ? desc : failure);
-                });
+        void switch_state(SuiteStates state)
+        {
+            if (state == SuiteStates::Main)
+            {
+                _ui.show();
+            }
+            else
+            {
+                assert(has_state(state));
+                _ui.hide();
+            }
+
+            _state = state;
+        }
+
+        template<class _State>
+        void add_state(SuiteStates state)
+        {
+            /* dont want to accidentally overwrite a state */
+            assert(!has_state(state));
+
+            _statemachine[state] = std::make_shared<_State>(_width, _height);
+            _statemachine[state]->start();
         }
     public:
         /* @param width: Window width
@@ -40,11 +105,16 @@ namespace suite
         PGPSuiteApplication(int width, int height) 
             : _height(height)
             , _width(width) 
-        {}
+            , _back_button({0.f,0.f,50.f,20.f})
+        {
+            _back_button.bind(ui::ButtonState::LeftClicked, [this]() { switch_state(SuiteStates::Main); });
+        }
 
         void start() override
         {
             init();
         }
+
+        bool has_state(SuiteStates state) const { return _statemachine.find(state) != _statemachine.end(); }
     };
 }
