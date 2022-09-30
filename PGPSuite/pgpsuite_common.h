49d0943a8fa218dd6173ca15ad088bc39e96828e
Lewi <privkoen00@gmail.com>
2022-09-30 15:55:38 +0200

    added a operation result for communicating errors

--- PGPSuite/pgpsuite_common.h
+++ PGPSuite/pgpsuite_common.h
@@ -2,3 +2,27 @@
 
 /* Used to check if an rnp function resulted in success */
 constexpr int RNP_SUCCESS{ 0 };
+
+namespace suite
+{
+	/* Operation Result
+	To be returned by pgp operations, if OpRes::what is not empty there is an error */
+	struct OpRes
+	{
+	private:
+		std::string _what;
+	public:	
+		OpRes() = default;
+		OpRes(std::string&& what)
+			: _what(what)
+		{}
+
+		operator bool() const { return !error(); }
+		bool error() const { return !_what.empty(); }
+
+		const auto& what() const
+		{
+			return _what;
+		}
+	};
+}
