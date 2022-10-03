a4f27716a206b0813804d24664ba96787ed38c79
Lewi <privkoen00@gmail.com>
2022-10-03 11:19:26 +0200

    struct OpRes: added implicit conversion to bool

--- PGPSuite/pgpsuite_common.h
+++ PGPSuite/pgpsuite_common.h
@@ -1,18 +1,25 @@
 #pragma once
 
+#include <string>
+
 /* Used to check if an rnp function resulted in success */
 constexpr int RNP_SUCCESS{ 0 };
 
-namespace suite
+namespace pgp
 {
 	/* Operation Result
-	To be returned by pgp operations, if OpRes::what is not empty there is an error */
+	To be returned by pgp operations, if OpRes::_what is not empty there is an error */
 	struct OpRes
 	{
 	private:
 		std::string _what;
 	public:	
 		OpRes() = default;
+		/* @param no_err: True if no error, False if error */
+		OpRes(bool no_err) 
+			: _what(no_err ? "" : "Generic error\n")
+		{}
+		/* @brief Constructs result with a custom message of what went wrong */
 		OpRes(std::string&& what)
 			: _what(what)
 		{}
