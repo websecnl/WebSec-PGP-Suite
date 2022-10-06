eb53c6fc6a53b0ca5d6d1f7cc0c6e4392731812a
Lewi <privkoen00@gmail.com>
2022-10-06 16:54:49 +0200

    isolated all enums

---
+++ PGPSuite/enums.h
@@ -0,0 +1,27 @@
+#pragma once
+
+namespace suite
+{
+    enum
+    {
+        ID_Hello = wxID_HIGHEST + 1,
+
+        /* id's to link button to input field */
+        ID_OPEN_PUBKEY,
+        ID_OPEN_SECKEY,
+        ID_OPEN_ENC_FILE,
+        ID_OPEN_FILE,
+        ID_EDIT_TEXT,
+
+        /* actions */
+        ID_GENERATE_KEY,
+        ID_ENCRYPT_FILE,
+        ID_DECRYPT_FILE,
+        ID_ENC_TYPE_RADIO_CHANGED,
+        ID_SHOW_GENERATE_SETTINGS,
+        ID_CHECK_VERSION,
+
+        /* unused */
+        ID_SAVE_FILE,
+    };
+}
