6c38f9373c6fb56faa7aae3413c32e908cd694a1
Lewi <privkoen00@gmail.com>
2022-10-19 14:47:33 +0200

    added method verify_local_version to do update checking

--- PGPSuite/versioning.h
+++ PGPSuite/versioning.h
@@ -13,6 +13,23 @@ namespace suite::ver
         return a == b;
     }
 
+    /* Checks if the local version is up-to-date, otherwise it displays a dialogue giving the latest version
+    @return If local version is up-to-date */
+    inline bool verify_local_version()
+    {
+        const auto local_version = get_local_version();
+        const auto latest_version = retrieve_version();
+
+        const auto res = compare(local_version, latest_version);
+
+        if (!res)
+        {
+            wxMessageBox(_("Current version: ") + local_version + _("\nNew version: ") + latest_version, _("New version available."));
+        }
+
+        return res;
+    }
+
     /* Will write a proper parser once it is required to receive more info from server, for now enjoy this monstrosity */
     inline std::string parse_version(const std::string& response)
     {
@@ -28,14 +45,22 @@ namespace suite::ver
         return response.substr(start + 1, end - start - 1);
     }
 
+    
     inline std::string retrieve_version()
     {
         const auto response = net::get_network_data();
+        auto version = parse_version(response);
 
-        return parse_version(response);
+        if (version.size() == 0)
+        {
+            wxMessageBox(_("Could not reach host, check your internet connection."), _("Error."), wxICON_ERROR);
+            return "";
+        }
+
+        return version;
     }
 
-    inline static wxString get_local_version()
+    inline wxString get_local_version()
     {
         wxFileInputStream fstream(_("version.txt"));
         wxTextInputStream input( fstream );
