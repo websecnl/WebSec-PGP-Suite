7ada707c5857acf364a42fa2cf6fc3cca15b8cbb
Lewi <privkoen00@gmail.com>
2022-10-21 18:01:38 +0200

    clearer error messages

--- PGPSuite/PGPDecrypt.cpp
+++ PGPSuite/PGPDecrypt.cpp
@@ -39,7 +39,7 @@ pgp::OpRes pgp::decrypt_text(std::string encrypted_file, std::string output_fnam
         /* load secret keyring, as it is required for public-key decryption. However, you may
             * need to load public keyring as well to validate key's signatures. */
         if (keyfile.set_input_from_path(secring_file)) 
-            return "Failed setting input for: " + secring_file;
+            return "Failed setting input for: " + secring_file + "\nDoes it exist?";
 
         if (rnp_load_keys(ffi, "GPG", keyfile, RNP_LOAD_SAVE_SECRET_KEYS) != RNP_SUCCESS)
             return "Failed to read secring.pgp\n";
@@ -49,15 +49,15 @@ pgp::OpRes pgp::decrypt_text(std::string encrypted_file, std::string output_fnam
 
     /* create file input and memory output objects for the encrypted message and decrypted
      * message */
-    if (input.set_input_from_path(std::forward<std::string>(encrypted_file)) != RNP_SUCCESS) return "Error setting input\n";
+    if (input.set_input_from_path(encrypted_file) != RNP_SUCCESS) return "Error setting input: " + encrypted_file + "\nDoes it exist?";
 
-    if (output.set_output_to_path(std::forward<std::string>(output_fname)) != RNP_SUCCESS) return "Error setting output\n";
+    if (output.set_output_to_path(output_fname) != RNP_SUCCESS) return "Error setting output: " + output_fname;
 
     /* input: where is the encrypted data
        output: where to save the decrypted data */
-    if (rnp_decrypt(ffi, input, output) != RNP_SUCCESS) 
+    if (auto res = rnp_decrypt(ffi, input, output); res != RNP_SUCCESS) 
     {
-        return "Decryption failed\n";
+        return "Decryption failed\nWas the password correct?\n";
     }
 
     return true;
