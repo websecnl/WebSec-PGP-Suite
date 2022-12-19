08de69c59d96aa25890305d8e2ad0c30a962dbd9
Lewi <privkoen00@gmail.com>
2022-12-19 13:01:56 +0100

    added copyright text

--- PGPSuite/QuickPromptOperations.h
+++ PGPSuite/QuickPromptOperations.h
@@ -1,3 +1,26 @@
+/*
+ *
+ * Copyright (c) 2018-2023
+ * Author: WebSec B.V.
+ * Developer: Koen Blok
+ * Website: https://websec.nl
+ *
+ * Permission to use, copy, modify, distribute this software
+ * and its documentation for non-commercial purposes is hereby granted exclusivley
+ * under the terms of the GNU GPLv3 License.
+ *
+ * Most importantly:
+ *  1. The above copyright notice appear in all copies and supporting documents.
+ *  2. The application / code will not be used or reused for commercial purposes.
+ *  3. All modifications are documented.
+ *  4. All new releases will remain open source and contain the same license.
+ *
+ * WebSec B.V. makes no representations about the suitability of this software for any
+ * purpose.  It is provided "as is" without express or implied warranty.
+ *
+ * please read the full license agreement for more information:
+ * https://github.com/websecnl/PGPSuite/LICENSE.md
+ */
 #pragma once
 
 #include "IOwx.h"
@@ -17,7 +40,7 @@ namespace suite
 		/* @brief Helper function to create a type _Item with a static string to the left
 		* @param static_text the static text to be set to the left of the item
 		* @param args the arguments of which _Item will be constructed
-		* @return the sizer with the static text and a the constructed item
+		* @return the sizer with the static text and the constructed item
 		*/
 		template<typename _Item, typename ... _Args>
 		inline std::pair<wxBoxSizer*, _Item*> create_formatted_item_with_text(wxPanel* parent, wxString static_text, _Args&&... args)
--- PGPSuite/pgpsuite_common.h
+++ PGPSuite/pgpsuite_common.h
@@ -1,3 +1,26 @@
+/*
+ *
+ * Copyright (c) 2018-2023
+ * Author: WebSec B.V.
+ * Developer: Koen Blok
+ * Website: https://websec.nl
+ *
+ * Permission to use, copy, modify, distribute this software
+ * and its documentation for non-commercial purposes is hereby granted exclusivley
+ * under the terms of the GNU GPLv3 License.
+ *
+ * Most importantly:
+ *  1. The above copyright notice appear in all copies and supporting documents.
+ *  2. The application / code will not be used or reused for commercial purposes.
+ *  3. All modifications are documented.
+ *  4. All new releases will remain open source and contain the same license.
+ *
+ * WebSec B.V. makes no representations about the suitability of this software for any
+ * purpose.  It is provided "as is" without express or implied warranty.
+ *
+ * please read the full license agreement for more information:
+ * https://github.com/websecnl/PGPSuite/LICENSE.md
+ */
 #pragma once
 
 #include <string>
